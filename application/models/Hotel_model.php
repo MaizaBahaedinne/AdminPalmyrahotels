625b8c7d940135863b9b0b7b591a65468250954f
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-01-24 17:52:56 +0100

    Update Hotel_model.php

--- application/models/Hotel_model.php
+++ application/models/Hotel_model.php
@@ -178,7 +178,41 @@ class Hotel_model extends CI_Model
 
 
 
- 
+             /**
+     * This function is used to get the user listing count
+     * @param string $searchText : This is optional search text
+     * @return number $count : This is row count
+     */
+    function Room(  $roomId   )
+    {
+
+        $this->db->select('BaseTbl.* , Room.* ');
+        $this->db->from('tbl_rooms as  BaseTbl');
+        $this->db->join('tbl_hotel_room as Room' , 'BaseTbl.roomId = Room.roomId','Left');           
+        $this->db->where('Room.hotel_roomId = ',$roomId );
+           
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+
+        /**
+     * This function is used to get the user listing count
+     * @param string $searchText : This is optional search text
+     * @return number $count : This is row count
+     */
+    function roomOptionsListing($opts)
+    {
+        $opts = str_replace("[", "(", $opts)  ;
+        $opts = str_replace("]", ")", $opts)  ;
+        $opts = str_replace("'", "", $opts)  ;
+        $this->db->select('BaseTbl.*  ');
+        $this->db->from('tbl_option as  BaseTbl');
+        $this->db->where('BaseTbl.optionId in '.$opts     );
+        $query = $this->db->get();
+     
+         return $query->result();
+    }
     
 
 }
