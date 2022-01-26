7fbb8b376fcf6d08780fc61e081fbcacd92e3e0f
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-01-26 19:55:36 +0100

    Update list.php

--- application/views/saison/list.php
+++ application/views/saison/list.php
@@ -68,7 +68,7 @@
 							</div>
 						</div>
 
-						<div class="col-6">
+						<div class="col-7">
 							<div class="card">
 								<div class="card-header">
 									<h5 class="card-title">Sasison Calendar</h5>
