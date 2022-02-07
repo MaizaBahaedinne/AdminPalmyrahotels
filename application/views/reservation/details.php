7831b31b19fbb11df47713108b6747ef7eb60be9
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-02-07 11:22:30 +0100

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -126,7 +126,7 @@
                                        </ul>
                                     </td>
                                     <td class="text-center"><?php echo $detail->price ?> <sup>DT</sup></td>
-                                     <td class="text-center"><?php echo $detail->price *  $reservation->nights ?> <sup>DT</sup></td>
+                                     <td class="text-center"><?php echo $detail->price *  $reservation->nights *  $reservation->adults  ?> <sup>DT</sup></td>
                                  </tr>
                                  <?php } ?>
 
