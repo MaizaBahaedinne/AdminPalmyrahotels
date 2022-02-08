3e04e47c590da0f79ef9c2faaa9abbf6aac7c7de
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-02-08 16:23:19 +0100

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -20,14 +20,15 @@
                      <button type="button" onclick="printJS('ivoic', 'html')">
                      Print Form
                      </button>
-                     <div class="content" style=" padding: 35px;border-radius: 30px;   background: url('https://www.palmyrahotels.tn/assets/img/logopageBG.png') ; background-repeat: no-repeat ; background-size: 50% auto; background-color: white; background-position: center; "  id="ivoic">
+                     <div class="content" style=" padding: 35px;border-radius: 30px;   background: url('https://www.palmyrahotels.tn/assets/img/logopageBG.webp' ; background-repeat: no-repeat ; background-size: 50% auto; background-color: white; background-position: center; "  id="ivoic">
                         <div class="container">
                            <div class="row">
                               <div class="col-12">
                                  <div class="invoice-title">
-                                                            <table width="100%">
+                                       
+                                        <table width="100%">
                            <tr>
-                              <td width="30%" ><img src="<?php echo base_url() ?>assets/img/logopage.png" width="150px"></td>
+                              <td width="30%" ><img src="https://www.palmyrahotels.tn/assets/img/logopage.png" width="150px"></td>
                               <td></td>
                               <td width="30%">
                                  <div id="qrcode" style="float:right;"></div>
@@ -48,7 +49,7 @@
                         </table>
                         <h3 class="pull-right">Order # <?php echo $reservation->reservationId ?></h3>
                         <h3 class="pull-right">Palmyra <?php echo $hotel->name ?></h3>
-                        <span class="pull-right"><?php echo $hotel->Adresse ?></h3>
+                        <span class="pull-right"><?php echo $hotel->Adresse ?></span>
                      </div>
                      <hr>
                      <table width="100%">
@@ -135,7 +136,7 @@
                                  <?php } ?>
                               </tbody>
                            </table>
-                           <table class="table "  width="100%"  >
+                           <table class=" "  width="100%"  >
                               <tbody>
                                  <tr>
                                     <td class="thick-line"></td>
@@ -171,84 +172,8 @@
 
                            Your reservation request will only be definitively confirmed after payment at the hotel </b>
                            <br>
-                           <b style="color:palevioletred ;"> Amount to pay at the hotel: <?php echo $reservation->price + ( $reservation->adult * 2)   ?>   <sup>DT</sup> </b>
+                           <b style="color:palevioletred ;"> Amount to pay at the hotel: <?php echo $reservation->price + ( $reservation->adult * 2)   ?>   <sup>DT</sup> </b>                                                         
 
-                              </div>
-                           </div>
-                        </div>
-                        <div class="row">
-                           <div class="col-lg-12">
-                              <div class="panel panel-default">
-                                 <div class="panel-heading">
-                                    <h3 class="panel-title"><strong>Order summary</strong></h3>
-                                 </div>
-                                 <div class="panel-body">
-                                    <div class="table-responsive">
-                                      <table class="table "  width="100%">
-                              <thead>
-                                 <tr>
-                                    
-                                    <td width="20%"><strong>Item</strong></td>
-                                    <td class="text-center" width="5%"><strong>Adult</strong></td>
-                                    <td class="text-center" width="5%"><strong>child</strong></td>
-                                    <td class="text-center" width="20%"><strong>Guests</strong></td>
-                                    <td class="text-center" width="25%"><strong>Options</strong></td>
-                                    <td class="text-center" width="10%" ><strong>Price</strong></td>
-                                    <td class="text-center" width="10%" ><strong>Total</strong></td>
-                                 </tr>
-                              </thead>
-                              <tbody>
-                                 <!-- foreach ($order->lineItems as $line) or some such thing here -->
-                                 <?php foreach ( $reservation->details as $detail ) {  ?>
-                                 <tr>
-                                    
-                                    <td><?php echo $detail->room->titre ?></td>
-                                    <td class="text-center"><?php echo $detail->adult ?></td>
-                                    <td class="text-center"><?php echo $detail->children ?></td>
-                                    <td> <?php foreach ( json_decode($detail->guests) as $gt) { echo '<li>'.$gt.'</li>' ; }  ?>   </td>
-                                    <td >
-                                       <ul>
-                                          <?php foreach ($detail->opts as $option ){ echo "<li>".$option->option."</li>" ; } ?>
-                                       </ul>
-                                    </td>
-                                    <td class="text-center"><?php echo $detail->price ?> <sup>DT</sup></td>
-                                     <td class="text-center"><?php echo $detail->price *  $reservation->nights *  $reservation->adult  ?> <sup>DT</sup></td>
-                                 </tr>
-                                 <?php } ?>
-                              </tbody>
-                           </table>
-                           <table class="table "  width="100%" >
-                              <tbody>
-                                 <tr>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line text-center"><strong>Sub total</strong></td>
-                                    <td class="thick-line text-right"><?php echo $reservation->price ?><sup>DT</sup></td>
-                                 </tr>
-                                 <tr>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="no-line text-center"><strong>Taxe</strong></td>
-                                    <td class="no-line text-right"><?php  echo $reservation->adult * 2 *  $reservation->nights ?><sup>DT</sup></td>
-                                 </tr>
-                                  <tr>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="thick-line"></td>
-                                    <td class="no-line text-center"><strong>Total</strong></td>
-                                    <td class="no-line text-right"><?php echo $reservation->price + ( $reservation->adult * 2) ?><sup>DT</sup></td>
-                                 </tr>
-                              </tbody>
-                           </table>
-                                    </div>
                                  </div>
                               </div>
                            </div>
@@ -258,7 +183,8 @@
                </div>
             </div>
          </div>
-         <div class="col-xxl-4">
+      </div>
+         <div class="col-4">
                 <div class="card">
                         <div class="card-header">
                                 <div class="card-actions float-end">
